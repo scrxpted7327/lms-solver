553c96974051b61fa33a593fb2cbad9161f6adcb
scrxpted <80987982+scrxpted7327@users.noreply.github.com>
2026-03-14 21:01:30 -0600

    chore: version to 2.0.3

--- install.js
+++ install.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         LMS AI Solver
-// @version      2.0.2
+// @version      2.0.3
 // @description  AI-powered solver for Mobius, Smartwork5, Canvas, and other LMS platforms
 // @namespace    http://tampermonkey.net/
 // @author       scrxpted7327
