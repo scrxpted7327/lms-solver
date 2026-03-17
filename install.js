12b8c5aeb46f170358351b3c6d28fb0bea990b8a
scrxpted <80987982+scrxpted7327@users.noreply.github.com>
2026-03-16 19:11:24 -0600

    Bump to v2.0.32

--- install.js
+++ install.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         LMS AI Solver
 // @namespace    http://tampermonkey.net/
-// @version      2.0.31
+// @version      2.0.32
 // @description  AI-powered solver for LMS platforms (Mobius, Smartwork5, Canvas)
 // @author       scrxpted7327
 // @match        *://*.mobius.cloud/*
