1cdc2d05794bff25d252587c44d899f666255f06
scrxpted <80987982+scrxpted7327@users.noreply.github.com>
2026-03-16 23:57:36 -0600

    Bump to v2.0.39 - checkbox multi-select fix

--- install.js
+++ install.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         LMS AI Solver
 // @namespace    http://tampermonkey.net/
-// @version      2.0.38
+// @version      2.0.39
 // @description  AI-powered solver for LMS platforms (Mobius, Smartwork5, Canvas)
 // @author       scrxpted7327
 // @match        *://*.mobius.cloud/*
